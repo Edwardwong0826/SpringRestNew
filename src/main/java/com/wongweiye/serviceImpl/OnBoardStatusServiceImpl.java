b6fc9db0689732708c5eb27a85013db4081c3031
Edwardwong0826 <edwardwong_94@outlook.com>
2023-06-04 20:44:23 +0800

    Spring transaction management info

--- src/main/java/com/wongweiye/serviceImpl/OnBoardStatusServiceImpl.java
+++ src/main/java/com/wongweiye/serviceImpl/OnBoardStatusServiceImpl.java
@@ -1,6 +1,10 @@
 package com.wongweiye.serviceImpl;
 
+import com.wongweiye.model.SystemParameter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.wongweiye.dto.SystemParameterIntDTO;
@@ -9,8 +13,16 @@ import com.wongweiye.service.OnBoardStatusService;
 
 import javax.transaction.Transactional;
 
+// https://juejin.cn/post/6844903608224333838 - Spring transaction management详解
+// can be enable using @Transactional annotation, or spring provides declarative transaction management by TransactionTemplate or TransactionManager manually manage transaction
+// @Transactional not recommended use on interface
+// due to mechanism of Spring AOP, it will create proxy via CGlib, unless target class implement interface then it will use JDK Dynamic proxy
+// avoid same class without annotated @Transactional method call @Transactional method, it will make transaction not work, this is because
+// Spring AOP proxy causing it, only being called by outside, the spring transaction management will work
+// or consider use AspectJ to replace Spring AOP proxy
+// annotated method class need to managed by spring, else not work
+// the implementation database engine need to support transaction mechanism
 @Service
-@Transactional
 public class OnBoardStatusServiceImpl implements OnBoardStatusService{
 
     @Autowired
@@ -29,16 +41,22 @@ public class OnBoardStatusServiceImpl implements OnBoardStatusService{
         }
     }
 
+    // @Transactional annotation is recommended put on method, and only work on public method
+    @Transactional
     public SystemParameterIntDTO findOnBoardStatus(String parGroup, String parName) {
 
 
         SystemParameterIntDTO result = systemParameterRepository.findGroupAndNameIntValue(parGroup,parName);
+        Pageable firstPageWithTwoElements = PageRequest.of(0, 2);
+        Page<SystemParameter> all = systemParameterRepository.findAll(firstPageWithTwoElements);
 
         if (result != null) {
 
             result.setDesc(Status.values()[result.getValue()].label);
 
         }
+
+
 
         return result;
 
