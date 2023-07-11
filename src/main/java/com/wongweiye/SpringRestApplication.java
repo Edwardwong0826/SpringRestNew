96beab2a49b7f5c65824f71013c80afd759dca7f
Edwardwong0826 <edwardwong_94@outlook.com>
2023-07-11 17:48:01 +0800

    Add spring context indexer

--- src/main/java/com/wongweiye/SpringRestApplication.java
+++ src/main/java/com/wongweiye/SpringRestApplication.java
@@ -2,17 +2,17 @@ package com.wongweiye;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Indexed;
 
-
+// @Indexed, this will pre create index for all the required component scan bean put into to the META-INF/spring.components file,
+// so when application started it will look at spring.components to load the beans, so can optimize a bit startup time
+@Indexed
 @SpringBootApplication
 public class SpringRestApplication {
-
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(SpringRestApplication.class, args);
-
-
 
 	}
 
